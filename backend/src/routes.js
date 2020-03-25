a5eeb639dd9795ccc43eb856794c00784925565f
Celso Junior <clsjunnior@live.com>
2020-03-25 20:00:41 -0300

    first-commit

---
+++ backend/src/routes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const crypto = require("crypto");
+const routes = express.Router();
+const connection = require("./database/connection");
+
+routes.get("/ongs", async (request, response) => {
+  const ongs = await connection("ongs").select("*");
+  return response.json(ongs);
+});
+
+routes.post("/ongs", async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+
+  const id = crypto.randomBytes(4).toString("HEX");
+
+  await connection("ongs").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+  return response.json({ id });
+});
+
+module.exports = routes;
