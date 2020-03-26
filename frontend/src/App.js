6c63b3b0d01af65fff6f3b9bc565efa21b6a272e
Celso Junior <clsjunnior@live.com>
2020-03-25 22:54:02 -0300

    [layout] - Paginas de Login e cadastro - frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React from 'react';
+import Routes from './routes';
+import './global.css';
+
 function App() {
-  return <h1>Olá</h1>;
+  return <Routes />;
 }
 
 export default App;
